76f960bf235bd5ebf2c1f24434c5be515005dc7c
Wojciech Królik <w.krolik@pilvin.pl>
2021-07-22 13:02:06 +0200

    cleaning up

--- src/home/urls.py
+++ src/home/urls.py
@@ -3,6 +3,4 @@ from django.urls import path
 from . import views
 
 # URLConf
-urlpatterns = [
-    path('hello/', views.say_hello)
-]
+urlpatterns = [path('', views.say_hello)]
--- src/home/views.py
+++ src/home/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 # request - > response
 # request handler / action
 
+
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Wojtek' })
+    return render(request, 'hello.html', {'name': 'Wojtek'})

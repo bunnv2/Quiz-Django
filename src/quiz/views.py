d402ec72e67fe86a354568945e32873959f27b61
Wojciech Królik <w.krolik@pilvin.pl>
2021-07-29 16:38:03 +0200

    ✨ Added quizes, questions and answers (displaying)

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question, Quiz
 
 
 def quizes(request):
-    return render(request, "quiz/quizes.html", {})
+    """Add all quizes to context and then iterate over them in template"""
+    quizes = Quiz.objects.all()
+    context = {
+        "quizes": quizes,
+    }
+    return render(request, "quiz/quizes.html", context)
+
+
+def quiz_view(request, quiz_id):
+    """Choose which quiz you want to be redirected to"""
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    questions = Question.objects.filter(quiz=quiz)
+
+    context = {
+        "quiz": quiz,
+        "questions": questions,
+    }
+    print(quiz.questions)
+    return render(request, "quiz/quiz_view.html", context)

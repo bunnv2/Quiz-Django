8c845a7e3a43b4cb50f4e86dd9f3b998e0b6e99f
Wojciech Królik <w.krolik@pilvin.pl>
2021-07-30 15:56:00 +0200

    ✨ Adds function that is prevents from inserting wrong values in quiz

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -16,10 +16,11 @@ def quiz_view(request, quiz_id):
     """Choose which quiz you want to be redirected to"""
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     questions = Question.objects.filter(quiz=quiz)
-
+    answers = [bool]
     context = {
         "quiz": quiz,
         "questions": questions,
+        "answers": answers,
     }
 
     if request.method != "POST":
@@ -27,8 +28,16 @@ def quiz_view(request, quiz_id):
 
     for question in questions:
         answer = request.POST.get(f"question_{question.id}", [None])[0]
-        if not answer or not question.is_answer_correct(int(answer)):
-            print(f"do better: {question.id}")
+
+        if not (answer == "0" or answer == "1" or answer == "2" or answer == "3" or answer == None):
+            print(f"nice try! ;) id: {answer}, question: {question.id}")
             continue
-        print(f"good job!: {question.id}")
+
+        if not answer or not question.is_answer_correct(int(answer)):
+            print(f"do better: id: {answer}, question: {question.id}")
+            continue
+
+        print(f"good job!: id: {answer}, question: {question.id}")
+        answers.append(True)
+
     return render(request, "quiz/quiz_view.html", context)

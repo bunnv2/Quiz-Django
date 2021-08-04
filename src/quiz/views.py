c774d6b2d8c7a20b0ab193cd863f678b834b5f01
Wojciech Królik <w.krolik@pilvin.pl>
2021-08-04 17:10:46 +0200

    🗃️ Time registering via db

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render
 
-from .models import Question, Quiz
+from .models import Question, Quiz, QuizResults
 
 
 def quizes(request):
@@ -13,7 +16,6 @@ def quizes(request):
 
 
 def quiz_view(request, quiz_id):
-    """Choose which quiz you want to be redirected to"""
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     questions = Question.objects.filter(quiz=quiz)
     context = {
@@ -22,11 +24,19 @@ def quiz_view(request, quiz_id):
     }
 
     if request.method != "POST":
+        session, created = QuizResults.objects.get_or_create(
+            quiz=quiz,
+            user=User.objects.get(id=request.user.id),
+            quiz_end=None,
+            defaults={"quiz_start": datetime.now()},
+        )
+
         return render(request, "quiz/quiz_view.html", context)
 
     results = 0
     answers_selected = {}
     answers_correct = {}
+
     for question in questions:
         answer = request.POST.get(f"question_{question.id}", [""])[0]
 
@@ -43,8 +53,16 @@ def quiz_view(request, quiz_id):
         answers_correct[question.id] = answer
         results += 1
 
+    quiz_result = results / questions.count() * 100
+
+    QuizResults.objects.filter(quiz=quiz, user=request.user, quiz_end=None).update(
+        quiz_end=datetime.now(),
+        results=quiz_result,
+    )
+
+    # dt =
     context["answers_selected"] = answers_selected
-    context["results"] = results / questions.count() * 100
+    context["results"] = quiz_result
     context["passed"] = quiz.is_quiz_passed(context["results"])
     context["questions"] = questions
     return render(request, "quiz/results_view.html", context)
